5d1760ef3a53f508bc8421bc07ade442e9563583
Dmitrij <dimaparfenyk1990@gmail.com>
2024-12-02 13:54:28 +0200

    refact server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,19 @@
+const mongoose = require("mongoose");
 const app = require("./app");
-const { connectDB } = require("./db/pets_db");
-const { PORT } = process.env;
+const { PORT, MONGODB_KEY } = process.env;
 
-app.listen(PORT, () => {
-  connectDB();
-  console.log(`Server work at ${PORT} port`);
-});
+// app.listen(PORT, async () => {
+//   try {
+//     await mongoose.connect(MONGODB_KEY);
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+// });
+
+mongoose
+  .connect(MONGODB_KEY)
+  .then(() => app.listen(PORT))
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
