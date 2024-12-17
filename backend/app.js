717ba89c7510721e64ed4d8133fb2bdc3cec5edd
Dmitrij <dimaparfenyk1990@gmail.com>
2024-12-18 00:40:04 +0200

    fix path to index.html

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "frontend", "public", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "frontend", "index.html"));
 });
 
 app.use("/api/auth", authRouter);
